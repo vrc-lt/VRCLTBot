f978c8704219c2212e4d865f483dd5bd3318ebab
sksat <sksat@sksat.net>
2022-01-09 17:29:06 +0900

    update: use temp_dir for split workspace on every request

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ use serenity::framework::standard::{
 };
 use serenity::model::channel::Message;
 
+use temp_dir::TempDir;
+
 extern crate reqwest;
 
 use bytes::Bytes;
 
 use std::fs;
+use std::path::Path;
 
 use std::env;
 use std::io::{BufWriter, Write};
@@ -40,17 +43,21 @@ impl EventHandler for Handler {
                 return;
             }
 
-            clean_up_tmp_dirs();
-            let _ = download_pdf(attachment_url.to_string()).await;
-            let _ = convert_pdf_to_png();
-            let paths = vec!["./downloaded/result.mp4"];
+            let dir = TempDir::new().expect("create temp directory failed");
+            let pdf = dir.path().join("download.pdf");
+            let _ = download_pdf(attachment_url.to_string(), &pdf).await;
+            let _ = convert_pdf_to_png(&pdf);
+            let mp4 = dir.path().join("result.mp4");
             println!("post file...");
             let _ = msg
                 .channel_id
-                .send_files(&ctx, paths, |m| m.content("変換しました"))
+                .send_files(&ctx, vec![&mp4], |m| m.content("変換しました"))
                 .await
                 .unwrap();
             println!("files sent.");
+
+            println!("creanup temp dir.");
+            drop(dir);
         }
     }
 }
@@ -89,24 +96,10 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     Ok(())
 }
 
-fn clean_up_tmp_dirs() {
-    let _ = Command::new("rm")
-        .arg("-rf")
-        .arg("downloaded")
-        .output()
-        .expect("failed to execute process")
-        .stdout;
-    let rm_downloaded = Command::new("mkdir")
-        .arg("downloaded")
-        .output()
-        .expect("failed to execute process")
-        .stdout;
-
-    println!("running rm: {:#?}", String::from_utf8(rm_downloaded));
-}
-fn convert_pdf_to_png() {
+fn convert_pdf_to_png(pdf: &Path) {
+    let dir = pdf.parent().unwrap();
     let _ = Command::new("pdftoppm")
-        .current_dir("downloaded")
+        .current_dir(dir)
         .arg("-scale-to-x")
         .arg("1280")
         .arg("-scale-to-y")
@@ -118,7 +111,7 @@ fn convert_pdf_to_png() {
         .expect("failed to execute process")
         .stdout;
     let exit_status = Command::new("ffmpeg")
-        .current_dir("downloaded")
+        .current_dir(dir)
         .arg("-y")
         .arg("-pattern_type")
         .arg("glob")
@@ -140,17 +133,19 @@ fn convert_pdf_to_png() {
     println!("conversion finished.");
 }
 
-async fn download_pdf(url_string: String) -> Result<Bytes, reqwest::Error> {
+async fn download_pdf(url_string: String, fname: &Path) -> Result<Bytes, reqwest::Error> {
     let client = reqwest::Client::new();
     let resp = client.get(&url_string).send().await?;
     let res_bytes = resp.bytes().await?;
     println!("pdf file downloaded.");
-    write_binary_file(res_bytes.clone()).unwrap();
+    write_binary_file(fname, res_bytes.clone()).unwrap();
     Ok(res_bytes)
 }
 
-fn write_binary_file(bytes: Bytes) -> std::io::Result<()> {
-    let mut file = BufWriter::new(fs::File::create("./downloaded/downloaded.pdf").unwrap());
+fn write_binary_file(fname: &Path, bytes: Bytes) -> std::io::Result<()> {
+    let file = fs::File::create(fname)
+        .unwrap_or_else(|_| panic!("create binary file failed: {}", fname.to_str().unwrap()));
+    let mut file = BufWriter::new(file);
     file.write_all(bytes.as_ref())?;
     println!("pdf file wrote.");
     Ok(())
